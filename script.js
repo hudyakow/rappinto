647041ac327c7fb8b956f7e9ec47dcb499371ab4
hudyakow.dev <108136177+hudyakow@users.noreply.github.com>
2023-07-26 16:22:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var para = document.getElementById("myDropdown");
+    para.classList.toggle("show");
+
+    var up = document.getElementById("up");
+    up.classList.toggle("rotate");
+}
